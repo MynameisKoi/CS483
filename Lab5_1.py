b9a4c5ea9cd9c0c9dce50cbca0e2cdd4faf47722
Khoi Duong <89900048+MynameisKoi@users.noreply.github.com>
2022-06-22 21:01:25 -0700

    Update Lab5_1.py

--- Lab5_1.py
+++ Lab5_1.py
@@ -64,3 +64,40 @@ ax1.set_xlabel('x axis')
 ax1.set_ylabel('y axis')
 ax1.set_zlabel('z axis')
 plt.show()
+
+# 4. 3D Bar Chart with Matplotlib
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import style
+style.use('ggplot')
+fig = plt.figure()
+ax1 = fig.add_subplot(111, projection='3d')
+x3 = [1,2,3,4,5,6,7,8,9,10]
+y3 = [5,6,7,8,2,5,6,3,7,2]
+z3 = np.zeros(10)
+dx = np.ones(10)
+dy = np.ones(10)
+dz = [1,2,3,4,5,6,7,8,9,10]
+ax1.bar3d(x3, y3, z3, dx, dy, dz)
+ax1.set_xlabel('x axis')
+ax1.set_ylabel('y axis')
+ax1.set_zlabel('z axis')
+plt.show()
+
+# 5. Conclusion
+import numpy as np
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+from matplotlib import style
+# %matplotlib notebook
+style.use('fivethirtyeight')
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1, projection = '3d')
+x, y, z = axes3d.get_test_data()
+# print(axes3d.__file__)
+ax1.plot_wireframe(x, y, z, rstride = 5, cstride = 5)
+ax1.set_xlabel('x axis')
+ax1.set_ylabel('y axis')
+ax1.set_zlabel('z axis')
+plt.show()
